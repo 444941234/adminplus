66c748ae145423cd11f511e5c9bcc8b08b7c98b3
wanglh <444941234@qq.com>
2026-04-03 17:43:55 +0800

    perf: upgrade RestTemplate with Apache HttpClient connection pooling
    - Switch from SimpleClientHttpRequestFactory to HttpComponentsClientHttpRequestFactory
    - Add connection request timeout (3s) to prevent connection pool exhaustion
    - Add httpclient5 dependency for connection pooling support

--- backend/src/main/java/com/adminplus/common/config/RestTemplateConfig.java
+++ backend/src/main/java/com/adminplus/common/config/RestTemplateConfig.java
@@ -2,11 +2,14 @@ package com.adminplus.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * RestTemplate 配置
+ * <p>
+ * 使用 Apache HttpClient 作为底层实现，支持连接池
+ * </p>
  *
  * @author AdminPlus
  * @since 2026-04-03
@@ -16,11 +19,13 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
         // 设置连接超时时间（5秒）
         factory.setConnectTimeout(5000);
         // 设置读取超时时间（30秒）
         factory.setReadTimeout(30000);
+        // 设置从连接池获取连接的超时时间（3秒）
+        factory.setConnectionRequestTimeout(3000);
         return new RestTemplate(factory);
     }
 }
