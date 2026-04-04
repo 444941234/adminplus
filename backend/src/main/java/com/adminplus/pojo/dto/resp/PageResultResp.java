7a1f45fedfd3b85f93651859ffc5b1a87ef45eb7
wanglh <444941234@qq.com>
2026-04-05 01:32:48 +0800

    feat: PageResultResp 添加 from(Page) 静态工厂方法
    简化从 Spring Data Page 到 DTO 的转换

--- backend/src/main/java/com/adminplus/pojo/dto/resp/PageResultResp.java
+++ backend/src/main/java/com/adminplus/pojo/dto/resp/PageResultResp.java
@@ -1,5 +1,6 @@
 package com.adminplus.pojo.dto.resp;
 
+import org.springframework.data.domain.Page;
 import java.util.List;
 
 /**
@@ -29,4 +30,21 @@ public record PageResultResp<T>(
          * 每页大小
          */
         Integer size
-) {}
+) {
+
+    /**
+     * 从 Spring Data Page 对象创建分页结果
+     *
+     * @param page Spring Data Page 对象
+     * @param <T>  数据类型
+     * @return 分页结果视图对象
+     */
+    public static <T> PageResultResp<T> from(Page<T> page) {
+        return new PageResultResp<>(
+                page.getContent(),
+                page.getTotalElements(),
+                page.getNumber() + 1,  // Page<number()> is 0-based
+                page.getSize()
+        );
+    }
+}
